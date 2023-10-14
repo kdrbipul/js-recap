eae047c6e6d62299a4953d85a2f57746f01b1529
kdrbipul <kdrbipul@gmail.com>
2023-10-15 00:43:57 +0600

    fist commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// let array1 = ['banana', 'mango']
+
+// let array2 = [...array1]
+// console.log(array2);
+
+
+// let array3 = ['banana', 'mango', 'apple']
+
+// let array4 = array3.slice(0,2);
+// console.log(array4);
+
+// let array1 = ['item1','item2','item3','item4','item5','item6','item7'];
+// let array2 = array1.slice(0, 6);
+// console.log(array2);
+
+// let array1 = ['item1', 'item2'];
+// let array2 = array1.slice(0).concat(['item3', 'item4']);
+// console.log(array2);
+
