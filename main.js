739a40735da2b528ffe7919461864fe1a5c855c9
kdrbipul <kdrbipul@gmail.com>
2023-10-22 09:19:38 +0600

    first commit

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
-// let array1 = ['banana', 'mango']
+// let array1 : ['banana', 'mango']
 
-// let array2 = [...array1]
+// let array2 : [...array1]
 // console.log(array2);
 
 
-// let array3 = ['banana', 'mango', 'apple']
+// let array3 : ['banana', 'mango', 'apple']
 
-// let array4 = array3.slice(0,2);
+// let array4 : array3.slice(0,2);
 // console.log(array4);
 
-// let array1 = ['item1','item2','item3','item4','item5','item6','item7'];
-// let array2 = array1.slice(0, 6);
+// let array1 : ['item1','item2','item3','item4','item5','item6','item7'];
+// let array2 : array1.slice(0, 6);
 // console.log(array2);
 
-// let array1 = ['item1', 'item2'];
-// let array2 = array1.slice(0).concat(['item3', 'item4']);
+// let array1 : ['item1', 'item2'];
+// let array2 : array1.slice(0).concat(['item3', 'item4']);
 // console.log(array2);
 
-// let array1 = ['item1','item2','item3']
-// let array2 = [].concat(array1,['item4']);
+// let array1 : ['item1','item2','item3']
+// let array2 : [].concat(array1,['item4']);
 // console.log(array2);
 
-// let array1 = ['item1','item2'];
-// let array2 = ['item3', 'item4'];
-// let array3 = [...array1, ...array2]
+// let array1 : ['item1','item2'];
+// let array2 : ['item3', 'item4'];
+// let array3 : [...array1, ...array2]
 // console.log(array3);
 
-// const fruits = ['item1','item2','item3'];
+// const fruits : ['item1','item2','item3'];
 
 // for (let fruit in fruits){
 //     console.log(fruits[fruit]);
 // } 
 
-// const person = {name:'abdul kader',
+// const person : {name:'abdul kader',
 //  age:23, 
 //  hobbies:[
 //     'caroom', 
@@ -44,8 +44,45 @@
 // console.log(person);
 // console.log(person.hobbies, person.hobbies[2]);
 
-const person = {name: 'abutl kader', age: 23, 'person hobbies' : [
-    'carrom', 'cooking', 'eating'
-]}
+// const person : {name: 'abutl kader', age: 23, 'person hobbies' : [
+//     'carrom', 'cooking', 'eating'
+// ]}
 
-console.log(person['person hobbies']);
+// console.log(person['person hobbies']);
+
+
+const users = [
+    {
+        userId : 1,
+        firstName : 'abdul',
+        lastName : 'kader',
+        age : 23,
+        gender : 'male'
+    },
+    {
+        userId : 1,
+        firstName : 'abdul',
+        lastName : 'baten',
+        age : 28,
+        gender : 'male'
+    },
+    {
+        userId :1,
+        firstName : 'mst',
+        lastName : 'mithila',
+        age : 20,
+        gender : 'female'
+    },
+    [
+        {
+            
+        }
+    ]
+];
+
+
+// const [{user1},{user2},{user3}] = users;
+// console.log(users);
+
+const [user1] = users;
+console.log(users);
