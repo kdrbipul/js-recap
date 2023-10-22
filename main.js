c7ff21dfe8b73f5861fe898578901826202f903c
kdrbipul <kdrbipul@gmail.com>
2023-10-22 10:31:37 +0600

    first commit

--- main.js
+++ main.js
@@ -108,14 +108,15 @@
 
 
 
-function isEven(number){
-    if (number % 2 === 0){
-        return true;
-    }else{
-        return false;
-    }
-}
+// function isEven(number){
+//     if (number % 2 === 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
 
 // console.log(number(5));
 
-console.log(isEven(5));
+// console.log(isEven(5));
+
